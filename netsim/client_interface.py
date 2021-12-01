c1b403ea93344b4b1df1136c5286945fab8be504
Isabel Zaller <izaller@princeton.edu>
2021-12-01 14:46:46 +0100

    full login functionality!

--- netsim/client_interface.py
+++ netsim/client_interface.py
@@ -12,8 +12,8 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_PSS
 
+# server's public key
 RSA_PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2KLWOGq+DKVqRaDqFzyk\n4oF37h/BnJsNJrK03id3x7KdFqv6Fzx3pptX7LJW3B5ECmQqG5tUhR7x+SLsIbNv\nWokruHVe8t8S0Y84Lg4eAbqu04+51wtgbX9wHQXHRaurSZn6LbFoZynPjvoaAAFW\nOPGDsnGYMFmZPRkUuk06q0/3hak8Rg1HGY1PDrVF54VSZ1w/Obj0n7WALFCHjtir\nk8aKQUfrS1+WVpqCz2HDtKUfJl4r4Tqs/abZwrsN5S4vpqy9MplBnnin2TUviRpi\n1Qpjsl2kT8MD0hUd8aX+qEy8aCn8NahztTyICQsfYDsBpZAVK5W12bJkqjAHUCA+\nGNTnUWBcujg1cApzOT/0p0AW/W0DNFoho1jyLhRj5FXTo+pTshweUceldjkZePwn\nT3b7LecvX42V35OELC0wsY/2t46PfKJKRHHxiAoBVrmxdhYJCEsSWAZ34Pzw+nQ8\nA5pXVHJLs7LWmFjWatyNhDRNhzEcRIKLv5OXEcDaqRaR3PXAz0JhsrQfenicJksZ\nMpb2ygdZFE2HoYgSkZvcRl2FOvoD7zFEFw5kTB34lcuVt6CzJ1O3M24X9qzLUP+Y\nTmANypBbMs8w+GlaUrD9T76C8oyyOxBH7QCisf2StUMwg1+V4W8LjGD4HFXP77bv\nHlgEe5RKf3cub7hWlQRGgWcCAwEAAQ==\n-----END PUBLIC KEY-----'
-RSA_PRIVATE_KEY = '-----BEGIN RSA PRIVATE KEY-----\nMIIJKQIBAAKCAgEA2KLWOGq+DKVqRaDqFzyk4oF37h/BnJsNJrK03id3x7KdFqv6\nFzx3pptX7LJW3B5ECmQqG5tUhR7x+SLsIbNvWokruHVe8t8S0Y84Lg4eAbqu04+5\n1wtgbX9wHQXHRaurSZn6LbFoZynPjvoaAAFWOPGDsnGYMFmZPRkUuk06q0/3hak8\nRg1HGY1PDrVF54VSZ1w/Obj0n7WALFCHjtirk8aKQUfrS1+WVpqCz2HDtKUfJl4r\n4Tqs/abZwrsN5S4vpqy9MplBnnin2TUviRpi1Qpjsl2kT8MD0hUd8aX+qEy8aCn8\nNahztTyICQsfYDsBpZAVK5W12bJkqjAHUCA+GNTnUWBcujg1cApzOT/0p0AW/W0D\nNFoho1jyLhRj5FXTo+pTshweUceldjkZePwnT3b7LecvX42V35OELC0wsY/2t46P\nfKJKRHHxiAoBVrmxdhYJCEsSWAZ34Pzw+nQ8A5pXVHJLs7LWmFjWatyNhDRNhzEc\nRIKLv5OXEcDaqRaR3PXAz0JhsrQfenicJksZMpb2ygdZFE2HoYgSkZvcRl2FOvoD\n7zFEFw5kTB34lcuVt6CzJ1O3M24X9qzLUP+YTmANypBbMs8w+GlaUrD9T76C8oyy\nOxBH7QCisf2StUMwg1+V4W8LjGD4HFXP77bvHlgEe5RKf3cub7hWlQRGgWcCAwEA\nAQKCAgAEvDZzgD3MN/dlMzm6rUwrWUoeTdplCmyUqnZSnsk697gATV0Hkf5JiX64\nAUPtxsmU8pCLDu+fg0glunIv4GSwNWxbuVEMfPzD0N4Y/5z/S63TmQImZazDZ/Qy\n5jPvXWu+jZuO0SBnlN3hEa1sz5qrXvgSd/IsKGE1mS0/Tz3XjcVqygKzw0mJEPIn\n76VqBJuC/9yv7c+HCCgXj+EOqcTCU6Yk/CXz/VBjbzgpwUjOU4elRoyt/SLX3oRH\nAeCxYDuRaJgwGXJ6PMfqcwOpewarAnGgbfSP+8LDHkwLwqWY1yiNHFHTeG2jCgIt\nNG2IpHGKRWZ5D+osKn6Ry7/1K+c/iGdlEmgB77oDHtVjnjnhjcaf/X5oi/APYkpD\nyI8ITcABBzvAFt0iyRNJw4FX0Fae89eo0kWJUACTfee+r1TjDjOuLNSInPVf1z22\n5lS/OURWWsSVkaZUlJaGwo9Y9oSLfeAf7C21muLT1tO9jEXNU7kjNc69AxzR93Sl\nuCRykfmFSh2Luq47XhpgVulnxDRodm/kcENJ1Z2jprw3IX56J79M/+bksxnx9xaC\nX5RqthoDYO1N0Ls4dXqkUGGnsyoqx3FMI4LOJmgmG4n4/uoalB9LPefo/Ahg9YH5\nUNfZ24sVz04lb9RXid/Coc4fKz6KLeebtFEpnjy1mWMhbEutdQKCAQEA5YGaRgEt\nF74vVecLCWB8tJeqEnCqIvzbQ9aOr3JIqZZH7dy3b8PjC+ucT+Uw/433wRDOsL7/\n4ntOsSwEZuwAaSddXedoZMWjelQpNOkjartHhHa3CdaNzTJiS+27c56BKVXqJN0f\nFzh8262Up1h/abjpdnaHrfev8ZJHPduUFPz5Toy5kM9/ANnO22y0CLsCGU9G0EsP\nOyLWS1jtMtj1TO5cJGb43ML3qj7kJ9KktdPW8iv4mN3U1+6MreXQOpP0h0ORaPH5\nIumeLPZjf9/mzY2T9kfCIS0l8gQr4f/MuRPe/5DWy/GQEzlzO+sqH4EDZwt7h8KN\n5gsUYf935Gf/RQKCAQEA8aTknq1WqxBBLfpzzLGxQ1tErbClI3d3VTzZarIDLxa1\nYywhyj8nH26kyDajAZ1Pw77TvjGK6LBTJMgZpJGlbilxgne/n4q9oRwjlRc1LrGg\nHn6CiVxGFVT698MV7Gt4Lg7xnriD4a70gLgu7y4e4NYJDaqvL5IrpA2x+hNqtfi9\neXvGTnvxsmyjnCifoa2BkBZVhvx9Ll3GyruhyBEM783eWfdIADepWSj8uHEgQsBz\nl0NooAlRmDLsLkKBmyI0kQZYGtJ0sr+ICoQvXk9zFlTS+wBtr4JAVMY6i/Okpi3p\n6Ys1WlE65wX20+JGSR5TVXHghS7edoqXGRdwJdaCuwKCAQEAmbs9lqTVCrFnTbhM\nqURLZECvOFjlbjhHu2IuA5Ge4JH8rnUJHsFtBaAV/WJ9dsEm8tkKSlQ2XQPRy1W7\nwSFWiRlILk2CnPXSMm/LhligU68NEcrfgqSIKaoVM90TkjfbNtAI3haL6+b3o8La\n71mVR0EIiUSOT9a4sS0VsXay83gcmyQibMDAxtYe/NYMpkh1+HQk8ANHOYp1VtVD\nVasEbTrA19Vt35ptgUlNVOuBTxaORXt0sxjsqJNvAlENMR/ITQ7SiSSEiIFKZb3J\nGm/lT00FjpO0krqGT13B80mAXXzVBAWGC+hMZMQ3zywP9DhcChsj7OVCXZSQW7Bf\nI80RrQKCAQEA6IDyAMNkCsBfFrBOz3uBxf+BO2Yl3tRKG9eqkoCpk5tT+BI0iPbu\n282H+6Smfx0v7HYmInBk6bMOrOtj0Pbap/50W0aBOC80eloq2n80CrOaDv1G+Iey\nX0AfIlmxNIPLZPW4AjIjovjGBTwy3KwRxd/rYh0C5tDL8NPElYwtNt4Y4VT43/dd\n/YGOguiLf/MEIPF8ZZ93iy9r4RFcfrX5Lpt1ADdwLdVguos3bvhaRCAMmFShzKpq\nufj1SyVusyfcUFY8W8J3yq2DZir9sM7dO3Vuc1hcMW0wHOGG37YUjFjNIotxUG+s\nLzGL5x9m5V+qayJhF4SbRI/hBqGIpL+blQKCAQASSYqyrFUhlO4KIZ7Z3AJvvFvR\n11cwKnIsWk1RoPgx6sWsAz5W++R7Ij/g+nTBKCVAKwS/nqGYh3eyjiZTOG0jk6fM\nCtblbGk36nvAZHIyH5ibJxtMhn+3Yx6wkVjLMvViJuj3y2gJjmAN5QvVi6ehkOQV\nR13AcEcCsSZC1Xlp2ptL8/n/pS0UgeEPh1aNi3WFkh38hJSQiRMftLm8zTVNKSou\n69onXg5gbS8lmMAN0hm308Enj8WObB9KqQy/jT91NG83ch4/P1qlekd1B4xYwds9\nm+yVezTLii5gJHQOrW1YzlKZsITUvzbJ+nyJZcaIybsQm9YOcttL6dfmcYCG\n-----END RSA PRIVATE KEY-----'
 
 server = 'S'
 LOGIN_SUCCESS = '1'
@@ -73,13 +73,16 @@ def build_msg(addr, inp):
     cmd_code = commands[cmd]
     return addr + cmd_code + arg
 
-# TODO: implement
-def public_encrypt(pswd):
-    return pswd
+def public_encrypt(pubkey, pswd):
+    rsa_cipher = PKCS1_OAEP.new(pubkey)
+    return rsa_cipher.encrypt(pswd.encode('utf-8'))
 
-# TODO: implement
-def sign(msg, sqn):
-    return ''
+# using RSA-PSS (dif from design doc, but works with other RSA throughout project)
+def sig_verified(pubkey, msg_signed, rsp_signature):
+    h = SHA512.new()
+    h.update(msg_signed.encode('utf-8'))
+    verifier = PKCS1_PSS.new(pubkey)
+    return verifier.verify(h, rsp_signature)
 
 def login(netif, addr):
     # input password in terminal
@@ -89,33 +92,31 @@ def login(netif, addr):
         pswd = input('Enter password: ')
         # encrypt password with public key
         # add check for password length? - can't be longer than RSA block size
-        pubkey = RSA.import_key(RSA_PUBLIC_KEY) # change how this is stored, just using as a placeholder
-        rsa_cipher = PKCS1_OAEP.new(pubkey)
-        cipher_pswd = rsa_cipher.encrypt(pswd.encode('utf-8'))
-        str_pswd = str(int.from_bytes(cipher_pswd, 'big'))
-        padded_pswd = str_pswd + 'x' * (1536 - len(str_pswd))
+        pubkey = RSA.import_key(RSA_PUBLIC_KEY)  # change how this is stored, just using as a placeholder
 
-        # build login request
+        #  login request:
         ## [address | login request | password | g^x mod p | sig(...)]
         ## one byte | one byte | 512 bytes | 256 bytes | ??
 
         # compute g^x mod p
         dh = pyDH.DiffieHellman()
         gxmodp = dh.gen_public_key()
-        msg = addr + commands['LOGIN'] + public_encrypt(padded_pswd) + str(gxmodp)
-        # TODO: sign message with MSN
-        sqn = ''
-        sig = sign(msg, sqn)
-        msg = msg + sig
+
+        # generate salt for HKDF
+        salt = get_random_bytes(16)
+
+        # build login message
+        encoded_pswd = public_encrypt(pubkey, pswd)
+        msg = (addr + commands['LOGIN']).encode('utf-8') + encoded_pswd + gxmodp.to_bytes(length=256, byteorder='big') + salt
 
         # send login request
-        netif.send_msg(server, msg.encode('utf-8'))
+        netif.send_msg(server, msg)
 
         # wait for server response
         status, rsp = netif.receive_msg(blocking=True)
 
         # parse response message for accept/reject
-        login_response = rsp.decode('utf-8')[0]     # get first byte
+        login_response = chr(rsp[0])     # get first byte
         if login_response == SERVER_UNAVAILABLE:
             print('The server is currently unavailable. Please try again later.')
             break
@@ -126,40 +127,22 @@ def login(netif, addr):
         # generate session key from DH key and store
         if password_accepted:
 
-            gymodp = int(rsp.decode('utf-8')[1:618])
+            gymodp = int.from_bytes(rsp[1:257], byteorder='big')
 
             # authenticate signature
-            # using RSA-PSS (dif from design doc, but works with other RSA throughout project)
             msg_signed = addr + str(gxmodp) + server + str(gymodp)
-            h = SHA512.new()
-            h.update(msg_signed.encode('utf-8'))
-            verifier = PKCS1_PSS.new(pubkey)
-
-            rsp_signature = rsp.decode('utf-8')[618:]
-            rsp_signature = rsp_signature[:rsp_signature.index('x')]
-            signature = int(rsp_signature).to_bytes(length=512, byteorder='big')
-
-            if verifier.verify(h, signature):
+            rsp_signature = rsp[257:]
+            if sig_verified(pubkey, msg_signed, rsp_signature):
                 print('Signature verified')
             else:
                 print('Signature verification failed')
                 break
 
-            # TODO: send final signed message w all DH parameters
-            sig = ''
-
-            # TODO: msg_final = [U | salt | sigU(addr | g^x mod p | S | g^y mod p)]
-            salt = get_random_bytes(16)
-            str_salt = str(int.from_bytes(salt, 'big'))
-            padded_salt = str_salt + 'x' * (48 - len(salt))
-            #msg_final = addr + str(int.from_bytes(salt, 'big')) + sig
-            msg_final = addr + padded_salt + sig
-            netif.send_msg(server, msg_final.encode('utf-8'))
-
+            # generate shared key from Diffie-Hellman parameters
             shared_key = dh.gen_shared_key(gymodp)
-            print(shared_key.encode('utf-8'))
+
+            # generate AES key with HKDF using shared key and salt
             AES_key = HKDF(shared_key.encode('utf-8'), 32, salt, SHA512, 1)
-            print('AES_key', AES_key)
 
             # create session and return user
             session = Session('S', AES_key)
--- netsim/server_interface.py
+++ netsim/server_interface.py
@@ -15,7 +15,7 @@ from session import Session
 
 RSA_PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA2KLWOGq+DKVqRaDqFzyk\n4oF37h/BnJsNJrK03id3x7KdFqv6Fzx3pptX7LJW3B5ECmQqG5tUhR7x+SLsIbNv\nWokruHVe8t8S0Y84Lg4eAbqu04+51wtgbX9wHQXHRaurSZn6LbFoZynPjvoaAAFW\nOPGDsnGYMFmZPRkUuk06q0/3hak8Rg1HGY1PDrVF54VSZ1w/Obj0n7WALFCHjtir\nk8aKQUfrS1+WVpqCz2HDtKUfJl4r4Tqs/abZwrsN5S4vpqy9MplBnnin2TUviRpi\n1Qpjsl2kT8MD0hUd8aX+qEy8aCn8NahztTyICQsfYDsBpZAVK5W12bJkqjAHUCA+\nGNTnUWBcujg1cApzOT/0p0AW/W0DNFoho1jyLhRj5FXTo+pTshweUceldjkZePwn\nT3b7LecvX42V35OELC0wsY/2t46PfKJKRHHxiAoBVrmxdhYJCEsSWAZ34Pzw+nQ8\nA5pXVHJLs7LWmFjWatyNhDRNhzEcRIKLv5OXEcDaqRaR3PXAz0JhsrQfenicJksZ\nMpb2ygdZFE2HoYgSkZvcRl2FOvoD7zFEFw5kTB34lcuVt6CzJ1O3M24X9qzLUP+Y\nTmANypBbMs8w+GlaUrD9T76C8oyyOxBH7QCisf2StUMwg1+V4W8LjGD4HFXP77bv\nHlgEe5RKf3cub7hWlQRGgWcCAwEAAQ==\n-----END PUBLIC KEY-----'
 RSA_PRIVATE_KEY = '-----BEGIN RSA PRIVATE KEY-----\nMIIJKQIBAAKCAgEA2KLWOGq+DKVqRaDqFzyk4oF37h/BnJsNJrK03id3x7KdFqv6\nFzx3pptX7LJW3B5ECmQqG5tUhR7x+SLsIbNvWokruHVe8t8S0Y84Lg4eAbqu04+5\n1wtgbX9wHQXHRaurSZn6LbFoZynPjvoaAAFWOPGDsnGYMFmZPRkUuk06q0/3hak8\nRg1HGY1PDrVF54VSZ1w/Obj0n7WALFCHjtirk8aKQUfrS1+WVpqCz2HDtKUfJl4r\n4Tqs/abZwrsN5S4vpqy9MplBnnin2TUviRpi1Qpjsl2kT8MD0hUd8aX+qEy8aCn8\nNahztTyICQsfYDsBpZAVK5W12bJkqjAHUCA+GNTnUWBcujg1cApzOT/0p0AW/W0D\nNFoho1jyLhRj5FXTo+pTshweUceldjkZePwnT3b7LecvX42V35OELC0wsY/2t46P\nfKJKRHHxiAoBVrmxdhYJCEsSWAZ34Pzw+nQ8A5pXVHJLs7LWmFjWatyNhDRNhzEc\nRIKLv5OXEcDaqRaR3PXAz0JhsrQfenicJksZMpb2ygdZFE2HoYgSkZvcRl2FOvoD\n7zFEFw5kTB34lcuVt6CzJ1O3M24X9qzLUP+YTmANypBbMs8w+GlaUrD9T76C8oyy\nOxBH7QCisf2StUMwg1+V4W8LjGD4HFXP77bvHlgEe5RKf3cub7hWlQRGgWcCAwEA\nAQKCAgAEvDZzgD3MN/dlMzm6rUwrWUoeTdplCmyUqnZSnsk697gATV0Hkf5JiX64\nAUPtxsmU8pCLDu+fg0glunIv4GSwNWxbuVEMfPzD0N4Y/5z/S63TmQImZazDZ/Qy\n5jPvXWu+jZuO0SBnlN3hEa1sz5qrXvgSd/IsKGE1mS0/Tz3XjcVqygKzw0mJEPIn\n76VqBJuC/9yv7c+HCCgXj+EOqcTCU6Yk/CXz/VBjbzgpwUjOU4elRoyt/SLX3oRH\nAeCxYDuRaJgwGXJ6PMfqcwOpewarAnGgbfSP+8LDHkwLwqWY1yiNHFHTeG2jCgIt\nNG2IpHGKRWZ5D+osKn6Ry7/1K+c/iGdlEmgB77oDHtVjnjnhjcaf/X5oi/APYkpD\nyI8ITcABBzvAFt0iyRNJw4FX0Fae89eo0kWJUACTfee+r1TjDjOuLNSInPVf1z22\n5lS/OURWWsSVkaZUlJaGwo9Y9oSLfeAf7C21muLT1tO9jEXNU7kjNc69AxzR93Sl\nuCRykfmFSh2Luq47XhpgVulnxDRodm/kcENJ1Z2jprw3IX56J79M/+bksxnx9xaC\nX5RqthoDYO1N0Ls4dXqkUGGnsyoqx3FMI4LOJmgmG4n4/uoalB9LPefo/Ahg9YH5\nUNfZ24sVz04lb9RXid/Coc4fKz6KLeebtFEpnjy1mWMhbEutdQKCAQEA5YGaRgEt\nF74vVecLCWB8tJeqEnCqIvzbQ9aOr3JIqZZH7dy3b8PjC+ucT+Uw/433wRDOsL7/\n4ntOsSwEZuwAaSddXedoZMWjelQpNOkjartHhHa3CdaNzTJiS+27c56BKVXqJN0f\nFzh8262Up1h/abjpdnaHrfev8ZJHPduUFPz5Toy5kM9/ANnO22y0CLsCGU9G0EsP\nOyLWS1jtMtj1TO5cJGb43ML3qj7kJ9KktdPW8iv4mN3U1+6MreXQOpP0h0ORaPH5\nIumeLPZjf9/mzY2T9kfCIS0l8gQr4f/MuRPe/5DWy/GQEzlzO+sqH4EDZwt7h8KN\n5gsUYf935Gf/RQKCAQEA8aTknq1WqxBBLfpzzLGxQ1tErbClI3d3VTzZarIDLxa1\nYywhyj8nH26kyDajAZ1Pw77TvjGK6LBTJMgZpJGlbilxgne/n4q9oRwjlRc1LrGg\nHn6CiVxGFVT698MV7Gt4Lg7xnriD4a70gLgu7y4e4NYJDaqvL5IrpA2x+hNqtfi9\neXvGTnvxsmyjnCifoa2BkBZVhvx9Ll3GyruhyBEM783eWfdIADepWSj8uHEgQsBz\nl0NooAlRmDLsLkKBmyI0kQZYGtJ0sr+ICoQvXk9zFlTS+wBtr4JAVMY6i/Okpi3p\n6Ys1WlE65wX20+JGSR5TVXHghS7edoqXGRdwJdaCuwKCAQEAmbs9lqTVCrFnTbhM\nqURLZECvOFjlbjhHu2IuA5Ge4JH8rnUJHsFtBaAV/WJ9dsEm8tkKSlQ2XQPRy1W7\nwSFWiRlILk2CnPXSMm/LhligU68NEcrfgqSIKaoVM90TkjfbNtAI3haL6+b3o8La\n71mVR0EIiUSOT9a4sS0VsXay83gcmyQibMDAxtYe/NYMpkh1+HQk8ANHOYp1VtVD\nVasEbTrA19Vt35ptgUlNVOuBTxaORXt0sxjsqJNvAlENMR/ITQ7SiSSEiIFKZb3J\nGm/lT00FjpO0krqGT13B80mAXXzVBAWGC+hMZMQ3zywP9DhcChsj7OVCXZSQW7Bf\nI80RrQKCAQEA6IDyAMNkCsBfFrBOz3uBxf+BO2Yl3tRKG9eqkoCpk5tT+BI0iPbu\n282H+6Smfx0v7HYmInBk6bMOrOtj0Pbap/50W0aBOC80eloq2n80CrOaDv1G+Iey\nX0AfIlmxNIPLZPW4AjIjovjGBTwy3KwRxd/rYh0C5tDL8NPElYwtNt4Y4VT43/dd\n/YGOguiLf/MEIPF8ZZ93iy9r4RFcfrX5Lpt1ADdwLdVguos3bvhaRCAMmFShzKpq\nufj1SyVusyfcUFY8W8J3yq2DZir9sM7dO3Vuc1hcMW0wHOGG37YUjFjNIotxUG+s\nLzGL5x9m5V+qayJhF4SbRI/hBqGIpL+blQKCAQASSYqyrFUhlO4KIZ7Z3AJvvFvR\n11cwKnIsWk1RoPgx6sWsAz5W++R7Ij/g+nTBKCVAKwS/nqGYh3eyjiZTOG0jk6fM\nCtblbGk36nvAZHIyH5ibJxtMhn+3Yx6wkVjLMvViJuj3y2gJjmAN5QvVi6ehkOQV\nR13AcEcCsSZC1Xlp2ptL8/n/pS0UgeEPh1aNi3WFkh38hJSQiRMftLm8zTVNKSou\n69onXg5gbS8lmMAN0hm308Enj8WObB9KqQy/jT91NG83ch4/P1qlekd1B4xYwds9\nm+yVezTLii5gJHQOrW1YzlKZsITUvzbJ+nyJZcaIybsQm9YOcttL6dfmcYCG\n-----END RSA PRIVATE KEY-----'
-RSA_BLOCK_SIZE = 512 * 3 # using 4096 when generating key
+RSA_BLOCK_SIZE = 512  # using 4096 when generating key
 
 CORRECT_PASSWORD = '1'
 LOGIN_FAILURE = '0'
@@ -54,7 +54,7 @@ class Serverif:
         plainstr = decrypt(msg, self.session.key)
 
         # get sender
-        addr = plainstr[0]
+        addr = chr(plainstr[0])
 
         # check server availability
         print('Checking server availability')
@@ -64,8 +64,9 @@ class Serverif:
             print('User ' + addr + ' tried to send message, but server is in session with user ' + self.session.partner)
             return
         print('Server available.')
+
         # get message command type and argument
-        cmd = str(plainstr[1])
+        cmd = chr(plainstr[1])
 
         if cmd == LOGIN:
             print('Login request received for user ' + addr)
@@ -104,18 +105,12 @@ def decrypt(msg, key):
     # cipher.update(header)
     # plaintext = cipher.decrypt_and_verify(ciphertext, tag)
     # return plaintext.decode('utf-8')
-    return msg.decode('utf-8')
-
-def public_decrypt(msg):
     return msg
 
 # TODO: check hash
 def correct_password(addr, pswd):
     keypair = RSA.import_key(RSA_PRIVATE_KEY)
     cipher = PKCS1_OAEP.new(keypair)
-
-    int_pswd = int(pswd[:pswd.index('x')])
-    pswd = int_pswd.to_bytes(length=512, byteorder='big')
     pswd = cipher.decrypt(pswd)
     return pswd.decode('utf-8') == PASSWORD
 
@@ -125,46 +120,34 @@ def sign(msg):
     h.update(msg.encode('utf-8'))
     key = RSA.import_key(RSA_PRIVATE_KEY)
     signer = PKCS1_PSS.new(key)
-    str_sig = str(int.from_bytes(signer.sign(h), 'big'))
-    sig = str_sig + 'x' * (RSA_BLOCK_SIZE - len(str_sig))
-    return sig
+    return signer.sign(h)
 
 def login(netif, msg):
-    sig_u = ''  # TODO: parse plainstr to get sig + authenticate
-
-    plainstr = public_decrypt(msg).decode('utf-8')
 
     # parse message
-    addr = plainstr[0]
-    pswd = str(plainstr[2:RSA_BLOCK_SIZE+2])
-    gxmodp = int(plainstr[RSA_BLOCK_SIZE+2:])
+    addr = chr(msg[0])
+    pswd = msg[2:RSA_BLOCK_SIZE+2]
+    gxmodp = int.from_bytes(msg[-272:-16], byteorder='big')
+    salt = msg[-16:]
 
     if correct_password(addr, pswd):    # check password
         # generate diffie-hellman parameters
         dh = pyDH.DiffieHellman()
         gymodp = dh.gen_public_key()    # compute g^y mod p
         msg_to_be_signed = addr + str(gxmodp) + 'S' + str(gymodp)
-        sig = sign(msg_to_be_signed)        # TODO: sign -- sigS(addr | g^x mod p | S | g^y mod p)
+        sig = sign(msg_to_be_signed)
 
         # response message: [correct password | g^y mod p | sigS(addr | g^x mod p | S | g^y mod p)]
-        ##                  12 bytes | 256 bytes | ?? ## TODO: pad passwords to uniform length
-        rsp = CORRECT_PASSWORD + str(gymodp) + sig
-        netif.send_msg(addr, rsp.encode('utf-8'))
+        ##                  12 bytes | 256 bytes | ??
+        rsp = CORRECT_PASSWORD.encode('utf-8') + gymodp.to_bytes(length=256, byteorder='big') + sig
+        netif.send_msg(addr, rsp)
 
-        # wait for final response from client with salt
-        status, rsp = netif.receive_msg(blocking=True)
-
-        # get salt
-        padded_salt = rsp.decode('utf-8')[1:49]
-        int_salt = int(padded_salt[:padded_salt.index('x')])
-        salt = int_salt.to_bytes(length=16, byteorder='big')
-
-        # TODO: authenticate signature
-
-        # generate session key from DH key and store
+        # generate shared key from Diffie-Hellman parameters
         shared_key = dh.gen_shared_key(gxmodp)  # compute shared key
+
+        # generate AES key with HKDF using shared key and salt
         AES_key = HKDF(shared_key.encode('utf-8'), 32, salt, SHA512, 1)
-        print('AES_key', AES_key)
+
         print('User ' + addr + ' logged in')
         return Session(addr, AES_key)
     else:
